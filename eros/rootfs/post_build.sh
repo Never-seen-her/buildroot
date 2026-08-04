cc9fab7f4e98e6ae3be0e01f2f4fb1a5f6d09ecd
eros <never.seen.h@gmail.com>
2026-08-04 22:12:07 +0800

    1.裁剪文件系统 rootfs.tar 3.3M
    libgfortran libgomp libm libstdc++
    
    2.增加镜像预处理post_build.sh脚本

---
+++ eros/rootfs/post_build.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# 文件: buildroot/board/eros/post-build.sh
+
+# $1 是 Buildroot 传递给脚本的目标文件系统路径 (output/target)
+TARGET_DIR="$1"
+
+# 删除 Fortran 运行时库 (868KB)
+#适用于科学计算和工程领域的高性能计算任务。
+rm -f "${TARGET_DIR}/usr/lib/libgfortran.so"*
+echo "Removed libgfortran"
+
+# 删除 OpenMP 库 (152KB)
+#数学函数（sin, cos, sqrt等）
+rm -f "${TARGET_DIR}/usr/lib/libgomp.so"*
+echo "Removed libgomp"
+
+# 如果你确定没有程序需要 libm，(624K)
+rm -f "${TARGET_DIR}/usr/lib/libm.so"*
+echo "Removed libm"
+
+# 1.5M
+rm -rf "${TARGET_DIR}/usr/lib/libstdc++.so"*
+echo "Removed C++ libraries"
+
+
+
+# 更多的清理规则可以按需添加...
+# 例如: rm -rf "${TARGET_DIR}/usr/share/doc"
