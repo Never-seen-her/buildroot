784dee6c5fca2f579d7de2020b5e5a50899fceb6
eros <never.seen.h@gmail.com>
2026-08-20 22:50:46 +0800

    add:1. profile PS1 2.ifconfig eth0; sshd 2222 port;

--- eros/rootfs/post_build.sh
+++ eros/rootfs/post_build.sh
@@ -1,33 +1,46 @@
 #!/bin/bash
 # 文件: buildroot/board/eros/post-build.sh
 
+set -x
 # $1 是 Buildroot 传递给脚本的目标文件系统路径 (output/target)
 TARGET_DIR="$1"
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 
+echo "SCRIPT_DIR:${SCRIPT_DIR} SCRIPT_DIR:${TARGET_DIR}"
+#==========================================================
+#           动态库 相关
+#==========================================================
 # 删除 Fortran 运行时库 (868KB)
 #适用于科学计算和工程领域的高性能计算任务。
 rm -f "${TARGET_DIR}/usr/lib/libgfortran.so"*
-echo "Removed libgfortran"
 
 # 删除 OpenMP 库 (152KB)
 #数学函数（sin, cos, sqrt等）
 rm -f "${TARGET_DIR}/usr/lib/libgomp.so"*
-echo "Removed libgomp"
 
 # 如果你确定没有程序需要 libm，(624K)
 # rm -f "${TARGET_DIR}/usr/lib/libm.so"*
-# echo "Removed libm"
 # ==> ethtool 需要 libm.so.6, 所以不能删除
 
 # 1.5M
 rm -rf "${TARGET_DIR}/usr/lib/libstdc++.so"*
-echo "Removed C++ libraries"
 
+#==========================================================
+#           etc 系统个性化配置 相关
+#==========================================================
+sh "${SCRIPT_DIR}/etc/profile_script" "${TARGET_DIR}"
+
+#==========================================================
+#           /etc/init.d 相关
+#==========================================================
+rm -f "${TARGET_DIR}/etc/init.d/S01seedrng"
 
 #清理etc？？？  是否有其他方法，可以避免安装S11moduels
 rm -rf "${TARGET_DIR}/etc/init.d/S11modules"
-echo "Removed S11modules"
 
-# 更多的清理规则可以按需添加...
-# 例如: rm -rf "${TARGET_DIR}/usr/share/doc"
+cp -f "${SCRIPT_DIR}/etc/init.d/S40network" "${TARGET_DIR}/etc/init.d/S40network"
+cp -f "${SCRIPT_DIR}/etc/init.d/S50sshd" "${TARGET_DIR}/etc/init.d/S50sshd"
+
+
+
 
